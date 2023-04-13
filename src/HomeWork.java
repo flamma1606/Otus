fbba5b85462ee8a01a43edaf57f80bffbbe2a5e5
YSuvorova <AflA_Lux0404>
2023-04-13 21:28:44 +0300

    Создание программы HomeWork.java

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,15 +1,5 @@
 import java.util.Base64;
 import java.util.Scanner;
-// Зарегистрироваться на github.com
-// Установить git
-// Установить JDK и среду разработки
-// Открыть данный класс в IDE
-// Скомпилировать данный класс с помощью команды javac.
-// Запустить программу на выполнение с помощью команды java
-// Запустить программу на выполнение с помощью среды разработки
-// Когда программа запросит - введите свое имя латиницей
-// Сделайте pull-request с программой.
-// Отправить в чат задания ссылку на pull-request
 public class HomeWork {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
