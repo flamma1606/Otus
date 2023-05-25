aacd21e00243902709e509ec08707cfddf8585b5
Суворова Яна Вячеславовна <YVSuvorova@alfabank.ru>
2023-05-25 08:52:17 +0300

    ДЗ по уроку 12 _ сортировка массива от 10_000 до 1

--- src/HomeWork12.java
+++ src/HomeWork12.java
@@ -1,17 +1,26 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.stream;
 
 public class HomeWork12 {
     public static void main(String[] args) {
 
-        System.out.println( "Cортировка пузырьком" );
-        long startTime1 = System.currentTimeMillis();
-        int[] array = new int[]
-                {
-                        1, 2, -6, 7, 6, 0, -7, -4, 5, -3, 3, 4, -5, -1, -2
-                };
+      int[] array = new int[10_000]; // Массив в обратном порядке
+              for(int i=0; i<10_000;i++){
+                  array[i] = 10_000 - i;
+              }
+ //     System.out.println("Массив в обратном порядке" + Arrays.toString(array) + "\n" );
 
+      List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList()); // Список в обратном порядке
+
+//      System.out.println("Список в обратном порядке" + list + "\n" );
+
+
+        System.out.println( "Cортировка пузырьком" );
+
+        long startTime1 = System.currentTimeMillis();
         int temp;
         boolean isSorted = false;
         while (!isSorted) {
@@ -30,11 +39,11 @@ public class HomeWork12 {
         long endTime1 = System.currentTimeMillis();
 
         // Результат выполнения сортировки пузырьком
-        System.out.println("Время выполнения: " + (endTime1 - startTime1) + " млс\n");
+        System.out.println("\nВремя выполнения: " + (endTime1 - startTime1) + " млс\n");
 
         System.out.println( "\nCортировка с помощью Collections.sort" );
         long startTime2 = System.currentTimeMillis();
-        List<Integer> list = Arrays.asList(1, 2, -6, 7, 6, 0, -7, -4, 5, -3, 3, 4, -5, -1, -2);
+
         Collections.sort(list);
         System.out.print(list + " ");
 
@@ -42,7 +51,7 @@ public class HomeWork12 {
 
         // Результат выполнения сортировки с помощью Collections.sort
 
-        System.out.println("Время выполнения: " + (endTime2 - startTime2) + " млс");
+        System.out.println("\nВремя выполнения: " + (endTime2 - startTime2) + " млс");
 
 
     }
